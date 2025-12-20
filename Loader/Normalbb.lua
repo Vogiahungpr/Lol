279d01df02e286f7452eebda68dbc2dcd71c4690
Vogiahungpr <djtg92x7v8@privaterelay.appleid.com>
2025-12-20 21:02:39 +0700

    Update Normalbb.lua

--- Loader/Normalbb.lua
+++ Loader/Normalbb.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/6e194a7ab670000e8249d3022af4976a321afbf0e1d45240575ef25daa0e7bfe/download"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/50951cea5ed8ec1c2c8f3d56bd2dda6e640c9c8f662ca5bdac5aa9701d4b64f6/download"))()
